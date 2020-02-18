771a869965c5ef0e10582ed955395f41f4949e95
devcoder111 <beautysky831@outlook.com>
2020-02-18 18:48:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+
+body {
+  font-family: "Oxygen", sans-serif !important;
+  margin: 0 !important;
+  padding: 0 !important;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,98 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+const Public = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/Public.vue");
+const Private = () =>
+  import(/* webpackChunkName: 'private' */ "@/views/Private.vue");
+const NotFound = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/NotFound.vue");
+
+const Login = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/Login/Login.vue");
+const ForgotPassword = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/Login/ForgotPassword.vue");
+const ResetPassword = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/Login/ResetPassword.vue");
+const SignUp = () =>
+  import(/* webpackChunkName: 'public' */ "@/views/Login/SignUp.vue");
+
+const Wizard = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/Index.vue");
+const WizardAbout = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/About.vue");
+const WizardUrl = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/Url.vue");
+const WizardInstallation = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/Installation.vue");
+const WizardSuccess = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/Success.vue");
+const WizardError = () =>
+  import(/* webpackChunkName: 'wizard' */ "@/views/Wizard/Error.vue");
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Public,
+      redirect: "login",
+      children: [
+        {
+          path: "login",
+          component: Login
+        },
+        {
+          path: "forgot-password",
+          component: ForgotPassword
+        },
+        {
+          path: "reset-password",
+          component: ResetPassword
+        },
+        {
+          path: "sign-up",
+          component: SignUp
+        },
+        {
+          path: "/wizard",
+          component: Wizard,
+          redirect: "/wizard/about",
+          children: [
+            {
+              path: "about",
+              component: WizardAbout,
+              meta: { step: 0 }
+            },
+            {
+              path: "url",
+              component: WizardUrl,
+              meta: { step: 1 }
+            },
+            {
+              path: "installation",
+              component: WizardInstallation,
+              meta: { step: 2 }
+            },
+            {
+              path: "success",
+              component: WizardSuccess
+            },
+            {
+              path: "error",
+              component: WizardError
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "*",
+      component: NotFound
+    }
+  ]
+});
+
+export default router;
